8931536a460d518be5c3a85a325db4a3b4209bcf
ayyoub benslimane <165728310+AyoubBenSlimane98@users.noreply.github.com>
2026-05-15 02:48:59 +0100

    setup backend docker and render deployment

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,18 +1,10 @@
-import dotenv from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-
-dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
-
-const url = process.env.DATABASE_URL;
-if (!url) {
-  throw new Error('DATABASE_URL is missing in drizzle.config');
-}
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/database/schema',
   dialect: 'postgresql',
   dbCredentials: {
-    url,
+    url: process.env.DATABASE_URL!,
   },
 });
